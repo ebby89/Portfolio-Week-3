9ffa96947448ffdee6e1cb4d4b1c5d03da96da5c
Kyle Ebersole <kyle.m.ebersole@gmail.com>
2017-09-22 14:04:11 -0700

    Add background image to container. Add javascript and jQuery. Update page styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,48 @@
-//BACK END
+// Back-End Business Logic
+var result = [];
 
+function playPingPong(number){
 
+  if (number<=0) {
+    alert("Please enter a positive number.");
+    return "";
+  }
 
+  for(var i = 1; i <= number; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      result.push("ping-pong");
+    } else if (i % 5 === 0) {
+      result.push("pong");
+    } else if (i % 3 === 0) {
+        result.push("ping");
+    } else {
+      result.push(i);
+    }
+  }
 
-//FRONT END
+  return result;
+
+};
+
+// Front-End User Interface
 $(document).ready(function(){
-  $("form#pingPongForm").submit(function(event){
+  $("form#pingPong").submit(function(event){
     event.preventDefault();
 
-    var number = parseInt($('#'))
+    var number = $("#numberInput").val()
 
+    $(".resultNumber").text(number);
+
+    var result = playPingPong(number)
+
+    result.forEach(function(item) {
+      $("ul").append("<li>"+item+"</li>");
+    });
+
+    $("#result").fadeIn(600);
+    $("#pingPong").hide();
+
+    var refreshPage = [];
 
   });
 });
